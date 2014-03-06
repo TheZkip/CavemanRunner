6d4863843155e752394f26f4d593ea5297437460
Kimmo Kaisla <kimmo.kaisla@gmail.com>
2014-03-06 18:32:00 +0200

    PoolingSystem.cs base

---
+++ CavemanRunner/CavemanRunner/PoolingSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CavemanRunner
+{
+    class PoolingSystem
+    {
+        private IList<GameObject> availableList = new List<GameObject>();
+        private IList<GameObject> inUseList = new List<GameObject>();
+        private GameObject tempGameObject;
+
+        public PoolingSystem (CavemanRunner game, int initialSize)
+        {
+            for (int i = 0; i < initialSize; i++)
+            {
+                //availableList.Add(new GameObject(game));
+            }
+        }
+
+        public GameObject GetElement ()
+        {
+            tempGameObject = availableList[0];
+            availableList.RemoveAt(0);
+
+            inUseList.Add(tempGameObject);
+
+            return tempGameObject;
+        }
+    }
+}
